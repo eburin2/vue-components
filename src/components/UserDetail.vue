872437a035e6c2763f7fcacc27dce3abf0b07abd
Elliot Burin <eburin2@gmail.com>
2018-07-02 17:59:32 -0700

    test

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ myName }}</p>
+        <p>User Age: {{ userAge }}</p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name</button>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      myName: {
+        type: String
+      },
+      resetFn: Function,
+      userAge: Number
+    },
+    methods: {
+      switchName(){
+      return this.myName.split("").reverse().join("");
+      },
+      resetName(){
+      this.myName = 'Elliot';
+      this.$emit('nameWasReset', this.myName);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
